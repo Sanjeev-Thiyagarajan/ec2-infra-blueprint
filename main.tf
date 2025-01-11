21f2b73e1fde9301c8433f5e38b9bcf6ca4467a1
sanjeev <sanjeevkt720@gmail.com>
2025-01-11 11:57:31 -0300

    first commit

--- main.tf
+++ main.tf
@@ -38,8 +38,8 @@ data "terraform_remote_state" "core" {
 module "my_ec2" {
   source        = "app.terraform.io/sanjeevkt720/ec2-infra/aws"
   version       = "1.0.0"
-  name          = "my-app-test"
-  instance_type = "t2.micro"
+  name          = "{{ values.name }}"
+  instance_type = "{{ values.instanceType }}"
   #   subnet        = data.tfe_outputs.core.values.subnet3_id
   subnet = data.terraform_remote_state.core.outputs.subnet3_id
 }
